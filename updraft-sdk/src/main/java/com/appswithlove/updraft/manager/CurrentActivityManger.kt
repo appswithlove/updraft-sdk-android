5b63d3dc9e914fe4113e7761b1b218bbe465778c
Sergey Chuvashev <chuvashev@appswithlove.com>
2024-06-25 15:53:36 +0400

    Fixed CurrentActivityManger

--- updraft-sdk/src/main/java/com/appswithlove/updraft/manager/CurrentActivityManger.kt
+++ updraft-sdk/src/main/java/com/appswithlove/updraft/manager/CurrentActivityManger.kt
@@ -15,7 +15,6 @@ class CurrentActivityManger private constructor() : Application.ActivityLifecycl
     }
 
     private var currentActivity: Activity? = null
-    private var currentLifecycle: LifecycleState? = null
 
     private val listeners: MutableSet<CurrentActivityListener> = mutableSetOf()
 
@@ -28,35 +27,30 @@ class CurrentActivityManger private constructor() : Application.ActivityLifecycl
         listeners.remove(listener)
     }
 
-    override fun onActivityCreated(activity: Activity, bundle: Bundle?) {
-        currentActivity = activity
-    }
+    override fun onActivityCreated(activity: Activity, bundle: Bundle?) {}
 
     override fun onActivityStarted(activity: Activity) {}
     override fun onActivityResumed(activity: Activity) {
-        currentLifecycle = LifecycleState.Resumed
+        currentActivity = activity
         dispatchCurrentActivityState()
     }
 
     override fun onActivityPaused(activity: Activity) {
-        currentLifecycle = LifecycleState.Paused
+        currentActivity = null
         dispatchCurrentActivityState()
     }
 
     override fun onActivityStopped(activity: Activity) {}
     override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {}
-    override fun onActivityDestroyed(activity: Activity) {
-        currentActivity = null
-        currentLifecycle = null
-    }
+    override fun onActivityDestroyed(activity: Activity) {}
 
     private fun dispatchCurrentActivityState() {
         val activity = currentActivity
         if (activity != null) {
             listeners.forEach { listener ->
-                if (currentLifecycle == LifecycleState.Resumed) {
+                if (currentActivity != null) {
                     listener.onActivityResumed(activity)
-                } else if (currentLifecycle == LifecycleState.Paused) {
+                } else {
                     listener.onActivityPaused(activity)
                 }
             }
@@ -66,10 +60,5 @@ class CurrentActivityManger private constructor() : Application.ActivityLifecycl
     interface CurrentActivityListener {
         fun onActivityResumed(activity: Activity)
         fun onActivityPaused(activity: Activity)
-    }
-
-    private enum class LifecycleState {
-        Resumed,
-        Paused
     }
 }
