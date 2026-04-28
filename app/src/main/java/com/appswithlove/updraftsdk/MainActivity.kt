5f5198f4afce1c19197c8f3a5316af277664647c
Yannick Pulver <yannick@appswithlove.com>
2026-04-28 16:21:16 +0200

    Remove update dialog test trigger from MainActivity

--- app/src/main/java/com/appswithlove/updraftsdk/MainActivity.kt
+++ app/src/main/java/com/appswithlove/updraftsdk/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.appswithlove.updraftsdk
 
-import android.content.DialogInterface
-import android.graphics.Typeface
+import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsControllerCompat
-import com.appswithlove.updraft.R as UpdraftR
 import com.appswithlove.updraftsdk.databinding.ActivityMainBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,19 +25,7 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
 
         binding.button.setOnClickListener {
-            val title = getString(UpdraftR.string.updraft_updateAvailable_titleWithVersion, "2.3.0")
-            val released = resources.getQuantityString(UpdraftR.plurals.updraft_relative_weeksAgo, 2, 2)
-            val message = getString(UpdraftR.string.updraft_updateAvailable_descriptionFull, released, "2.2.5")
-            val dialog = MaterialAlertDialogBuilder(this)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(UpdraftR.string.updraft_updateAvailable_openButton, null)
-                .setNegativeButton(UpdraftR.string.updraft_updateAvailable_laterButton, null)
-                .setCancelable(false)
-                .show()
-            dialog.findViewById<TextView>(androidx.appcompat.R.id.alertTitle)?.apply {
-                typeface = Typeface.create(typeface, Typeface.BOLD)
-            }
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
