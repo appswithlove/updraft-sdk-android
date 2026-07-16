0deb8b8157c5def5eeafb72a8453b8874bd1f016
Michel Utke <utke.michel@gmail.com>
2026-07-16 11:24:40 +0200

    Add UpdraftSettings and LogLevel to commonMain

---
+++ updraft-core/src/commonMain/kotlin/com/appswithlove/updraft/UpdraftSettings.kt
@@ -0,0 +1,21 @@
+package com.appswithlove.updraft
+
+enum class LogLevel { None, Error, Debug }
+
+class UpdraftSettings(
+    val appKey: String,
+    val sdkKey: String,
+    val baseUrl: String = BASE_URL_PROD,
+    val logLevel: LogLevel = LogLevel.Error,
+    val showFeedbackAlert: Boolean = true,
+    val feedbackEnabled: Boolean = true,
+    val storeRelease: Boolean = false,
+) {
+    fun shouldShowErrors(): Boolean =
+        logLevel == LogLevel.Error || logLevel == LogLevel.Debug
+
+    companion object {
+        const val BASE_URL_PROD = "https://app.getupdraft.com/api/"
+        const val BASE_URL_STAGING = "https://u2.mqd.me/api/"
+    }
+}
